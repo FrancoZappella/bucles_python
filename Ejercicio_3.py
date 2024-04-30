87782ef61c069ec88f3946c6f908fcf908d1c620
FrancoZappella <103602911+FrancoZappella@users.noreply.github.com>
2024-04-30 00:11:00 -0300

    Add files via upload

---
+++ Ejercicio_3.py
@@ -0,0 +1,22 @@
+
+# CODE:20
+# IMPORTANTE: NO borrar los comentarios
+
+print("Desafio con listas")
+
+# Alumno:
+# Crear una variable del tipo lista
+# llamada contenidos_curso
+# En dicha variable deberá almacenar los siguientes
+# elementos tipo texto en el siguiente orden:
+
+# python
+# variables
+# condicionales
+# listas
+# bucles
+contenidos_curso = ['python', 'variables', 'condicionales', 'listas', 'bucles']
+
+
+# Imprimir en pantalla la variable contenidos_curso
+print(contenidos_curso)
